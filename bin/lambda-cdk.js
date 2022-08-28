60bf1bf77fbace1dd4ac14bc81b6707572d2d520
Durvesh <durvesh09@gmail.com>
2022-08-28 07:27:36 +0530

    Initial commit

---
+++ bin/lambda-cdk.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const cdk = require('aws-cdk-lib');
+const { LambdaCdkStack } = require('../lib/lambda-cdk-stack');
+
+const app = new cdk.App();
+new LambdaCdkStack(app, 'LambdaCdkStack');
